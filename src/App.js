65e57a222eebd81b2682b32c02bbbd6ccdaafdc8
unknown <duchi@aol.com>
2021-04-28 11:33:53 -1000

    added a few new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
-//import './App.css';
+import './App.css';
+import Hero from './components/Hero';
 import Header from  './components/Header';
+import SubscriptionLink from './components/SubscriptionLink';
 import Footer from  './components/Footer';
 import Team from  './components/Team';
 
 function App() {
   return (
     <div>
-asdlfkjs
-<Header/>;
-<Team/>;
-<Footer/>;
+      <Hero/>
+      <Header/>
+      <SubscriptionLink/>
+      <Team/>
+      <Footer/>
     </div>
   );
 }
